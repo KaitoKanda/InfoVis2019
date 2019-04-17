8c8c824d0dfca0f6b851fdbdaec7827a45228538
KaitoKanda <49426747+KaitoKanda@users.noreply.github.com>
2019-04-17 14:49:41 +0900

    Add files via upload

--- w04/task2/main.js
+++ w04/task2/main.js
@@ -10,29 +10,32 @@ function main()
     var near = 1;
     var far = 1000;
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );
+    camera.position.set( 0, 0, 10 );
     scene.add( camera );
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
+    
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    var light = new THREE.PointLight( 0xff0123 );
-    light.position.set( 1, 1, 1 );
+    //var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var material = new THREE.MeshLambertMaterial({
+    color: 0xf54321
+    });
+    var light = new THREE.PointLight( 0xff1234 );
+    light.position.set( 4, 1, 1 );
     scene.add( light );
+    var cube = new THREE.Mesh( geometry, material);
+    scene.add( cube );
 
     loop();
 
     function loop()
     {
         requestAnimationFrame( loop );
-        cube.rotation.x += 0.001;
-        cube.rotation.y += 0.001;
+        cube.rotation.x += 0.005;
+        cube.rotation.y += 0.005;
         renderer.render( scene, camera );
     }
 }
